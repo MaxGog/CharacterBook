5af1d906ea9e60373adbf44cec2e6857f389fef8
MaxGog <max.gog2005@outlook.com>
2025-04-24 19:51:26 +0300

    Добавление файлов локализации (русский и английский)

---
+++ CharacterBook.Flutter/characterbook/lib/generated/intl/messages_en.dart
@@ -0,0 +1,47 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "add_picture": MessageLookupByLibrary.simpleMessage("Add picture"),
+        "age": MessageLookupByLibrary.simpleMessage("Age"),
+        "another": MessageLookupByLibrary.simpleMessage("Another"),
+        "app_name": MessageLookupByLibrary.simpleMessage("CharacterBook"),
+        "appearance": MessageLookupByLibrary.simpleMessage("Appearance"),
+        "biography": MessageLookupByLibrary.simpleMessage("Biography"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
+        "character": MessageLookupByLibrary.simpleMessage("Character"),
+        "copy": MessageLookupByLibrary.simpleMessage("Copy"),
+        "create": MessageLookupByLibrary.simpleMessage("Create"),
+        "edit": MessageLookupByLibrary.simpleMessage("Edit"),
+        "empty_list": MessageLookupByLibrary.simpleMessage("Here empty!"),
+        "export": MessageLookupByLibrary.simpleMessage("Export"),
+        "female": MessageLookupByLibrary.simpleMessage("Female"),
+        "gender": MessageLookupByLibrary.simpleMessage("Gender"),
+        "male": MessageLookupByLibrary.simpleMessage("Male"),
+        "name": MessageLookupByLibrary.simpleMessage("Name"),
+        "new_character": MessageLookupByLibrary.simpleMessage("New character"),
+        "ok": MessageLookupByLibrary.simpleMessage("OK"),
+        "personality": MessageLookupByLibrary.simpleMessage("Personality"),
+        "save": MessageLookupByLibrary.simpleMessage("Save")
+      };
+}
---
+++ CharacterBook.Flutter/characterbook/lib/generated/l10n.dart
@@ -0,0 +1,288 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+
+class S {
+  S();
+
+  static S? _current;
+
+  static S get current {
+    assert(_current != null,
+        'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
+
+  static Future<S> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = S();
+      S._current = instance;
+
+      return instance;
+    });
+  }
+
+  static S of(BuildContext context) {
+    final instance = S.maybeOf(context);
+    assert(instance != null,
+        'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
+    return instance!;
+  }
+
+  static S? maybeOf(BuildContext context) {
+    return Localizations.of<S>(context, S);
+  }
+
+  /// `CharacterBook`
+  String get app_name {
+    return Intl.message(
+      'CharacterBook',
+      name: 'app_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Male`
+  String get male {
+    return Intl.message(
+      'Male',
+      name: 'male',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Female`
+  String get female {
+    return Intl.message(
+      'Female',
+      name: 'female',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Another`
+  String get another {
+    return Intl.message(
+      'Another',
+      name: 'another',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Name`
+  String get name {
+    return Intl.message(
+      'Name',
+      name: 'name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Age`
+  String get age {
+    return Intl.message(
+      'Age',
+      name: 'age',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Gender`
+  String get gender {
+    return Intl.message(
+      'Gender',
+      name: 'gender',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Biography`
+  String get biography {
+    return Intl.message(
+      'Biography',
+      name: 'biography',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Character`
+  String get character {
+    return Intl.message(
+      'Character',
+      name: 'character',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Personality`
+  String get personality {
+    return Intl.message(
+      'Personality',
+      name: 'personality',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Appearance`
+  String get appearance {
+    return Intl.message(
+      'Appearance',
+      name: 'appearance',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Cancel`
+  String get cancel {
+    return Intl.message(
+      'Cancel',
+      name: 'cancel',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Copy`
+  String get copy {
+    return Intl.message(
+      'Copy',
+      name: 'copy',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `OK`
+  String get ok {
+    return Intl.message(
+      'OK',
+      name: 'ok',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Export`
+  String get export {
+    return Intl.message(
+      'Export',
+      name: 'export',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Here empty!`
+  String get empty_list {
+    return Intl.message(
+      'Here empty!',
+      name: 'empty_list',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Edit`
+  String get edit {
+    return Intl.message(
+      'Edit',
+      name: 'edit',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Create`
+  String get create {
+    return Intl.message(
+      'Create',
+      name: 'create',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `New character`
+  String get new_character {
+    return Intl.message(
+      'New character',
+      name: 'new_character',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Add picture`
+  String get add_picture {
+    return Intl.message(
+      'Add picture',
+      name: 'add_picture',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Save`
+  String get save {
+    return Intl.message(
+      'Save',
+      name: 'save',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<S> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
