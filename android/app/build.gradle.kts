5b5d2bb5db4f7e525681976616d02351b3922532
MaxGog <max.gog2005@outlook.com>
2026-05-13 13:18:46 +0300

    Обновление конфигурации Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,8 +25,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     defaultConfig {
@@ -39,23 +41,20 @@ android {
 
     signingConfigs {
         create("release") {
-            if (keystorePropertiesFile.exists()) {
-                storeFile = file(keystoreProperties["storeFile"] as String)
-                storePassword = keystoreProperties["storePassword"] as String
-                keyAlias = keystoreProperties["keyAlias"] as String
-                keyPassword = keystoreProperties["keyPassword"] as String
-            } else {
-                storeFile = file("keystore.jks")
-                storePassword = "2404Bhbyf1975"
-                keyAlias = "upload"
-                keyPassword = "2404Bhbyf1975"
-            }
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
         }
     }
 
     buildTypes {
-        release {
-            signingConfig = signingConfigs["release"]
+        getByName("release") {
+            signingConfig = if (keystorePropertiesFile.exists()) {
+                signingConfigs.getByName("release")
+            } else {
+                signingConfigs.getByName("debug")
+            }
             isShrinkResources = true
             isMinifyEnabled = true
             proguardFiles(
